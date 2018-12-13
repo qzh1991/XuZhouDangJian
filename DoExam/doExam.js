7cdd3fb9ce9f738c836228c0e9d1abad9e29bb1e
qzh1991 <qzh1991@qq.com>
2018-12-13 16:20:49 +0800

    1

--- DoExam/doExam.js
+++ DoExam/doExam.js
@@ -5,11 +5,11 @@ function main() {
         console.log('网页错误')
         return
     }
-    let questions = main.find('td[class="hs11"]:even');
-    let answers = main.find('td[class="hs11"]:odd');
+    let questionsArray = main.find('td[class="hs11"]:even');
+    let optionsArray = main.find('td[class="hs11"]:odd');
     let array = []
-    for (let i = 0; i < questions.length; i++) {
-        let v = questions[i];
+    for (let i = 0; i < questionsArray.length; i++) {
+        let v = questionsArray[i];
         let lineTxt = $(v).text().trim();
         lineTxt = lineTxt.replace(/\s+/g, '');
         array.push(lineTxt);
@@ -24,28 +24,41 @@ function main() {
             'name': 'XuZhouDangJian',
             'text': JSON.stringify(array)
         },
-        success: function(r) {
-            console.log(r)
-            for (let i = 0; i < answers.length; i++) {
-                let answer = answers[i];
-                let answerInput = $(answer).find('input');
-                let answerText = $(answer).find('label');
-                let a = r[i]
-                if (a.s == 1) {
-                    for (let j = 0; j < answerText.length; j++) {
-                        let v = $(answerText[j]).text();
-                        if ((a.a).indexOf(v.slice(0, 1)) >= 0 ||
-                            (a.a).indexOf(v.slice(v.length - 1, v.length)) >= 0) {
-                            $(answerInput[j]).attr('checked', 'true');
-                            $(answerInput[j])[0].checked = true;
-                        };
-                    };
+        success: function(results) {
+            console.log(results)
+            for (let i = 0; i < results.length; i++) {
+                let result = results[i]
+                if (result.s == 1) {
+                    let answerIndex = result.a - 65
+                    let optionsInput = $(optionsArray[i]).find('input');
+                    $(optionsInput[answerIndex]).attr('checked', 'true');
+                    $(optionsInput[answerIndex])[0].checked = true;
+                    //let answerText = $(optionsArray[i]).find('label');
                 }else{
-                    window.prompt('以下题目未找到答案，请手动选择。',a.a);
+                    window.prompt('以下题目未找到答案，请手动选择。',option.a);
                 }
-            };
-        },
-    });
+            }
+
+            // for (let i = 0; i < optionsArray.length; i++) {
+            //     let options = optionsArray[i];
+            //     let answerInput = $(options).find('input');
+            //     let answerText = $(options).find('label');
+            //     let option = r[i]
+            //     if (option.s == 1) {
+            //         for (let j = 0; j < answerText.length; j++) {
+            //             let v = $(answerText[j]).text();
+            //             if ((option.a).indexOf(v.slice(0, 1)) >= 0 ||
+            //                 (option.a).indexOf(v.slice(v.length - 1, v.length)) >= 0) {
+            //                 $(answerInput[j]).attr('checked', 'true');
+            //                 $(answerInput[j])[0].checked = true;
+            //             };
+            //         };
+            //     }else{
+            //         window.prompt('以下题目未找到答案，请手动选择。',option.a);
+            //     }
+            // }
+        }
+    })
 
 }
 $(function() {
